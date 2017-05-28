31a6f4780665d77510f9092bfc419c978eb8a97a
dov <dov@dov-Inspiron-5520>
2017-05-27 23:35:49 -0400

    Merge origin/master
    Conflicts:
    	README.md

---
+++ Pong/src/main/java/Scraper/Main.java
@@ -0,0 +1,11 @@
+package Scraper;
+
+import ScraperClass.ScrapeUrlAndEmail;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ScrapeUrlAndEmail sUAE = new ScrapeUrlAndEmail("https://www.google.com/search?client=ubuntu&channel=fs&q=buchbinder&ie=utf-8&oe=utf-8", 10);
+    }
+
+}
